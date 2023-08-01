94955ee637615cbc485aa82d58a257eee4626d97
lab206 <132445267+lab206@users.noreply.github.com>
2023-08-01 20:43:11 +0800

    Update losses.py

--- isegm/model/losses.py
+++ isegm/model/losses.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 
 
 class BalancedNormalizedFocalLossSigmoid(nn.Module):
-    def __init__(self, axis=-1, varepsilon=0.8, gamma=2, max_mult=-1, eps=1e-12,
+    def __init__(self, axis=-1, varepsilon=0.8, alpha=0.5, gamma=2, max_mult=-1, eps=1e-12,
                  from_sigmoid=False, detach_delimeter=True,
                  batch_axis=0, weight=None, size_average=True,
                  ignore_label=-1):
@@ -26,11 +26,13 @@ class BalancedNormalizedFocalLossSigmoid(nn.Module):
         self._max_mult = max_mult
 
     def forward(self, pred, label):
+        one_hot = label > 0.5
         sample_weight = label != self._ignore_label
 
         if not self._from_logits:
             pred = torch.sigmoid(pred)
 
+        alpha = torch.where(one_hot, self._alpha * sample_weight, (1 - self._alpha) * sample_weight)
         pt = torch.where(sample_weight, 1.0 - torch.abs(label - pred), torch.ones_like(pred))
         beta = (1 - pt) ** self._gamma
 
@@ -47,7 +49,7 @@ class BalancedNormalizedFocalLossSigmoid(nn.Module):
         if self._max_mult > 0:
             beta = torch.clamp_max(beta, self._max_mult)
 
-        focal_loss = - beta * torch.log(torch.min(pt + self._eps, torch.ones(1, dtype=torch.float).to(pt.device)))
+        focal_loss = - alpha * beta * torch.log(torch.min(pt + self._eps, torch.ones(1, dtype=torch.float).to(pt.device)))
         dif_imbalance_loss = kappa * focal_loss
 
         loss = self._weight * (dif_imbalance_loss * sample_weight + num_imbalance_loss)
